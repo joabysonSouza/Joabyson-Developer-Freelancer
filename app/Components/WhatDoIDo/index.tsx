12a28235decb02fcb3296ad1de89fa24487bf211
Joabyson <joabyson916@gmail.com>
2024-10-16 23:11:03 -0300

    alterando erro de digitação

--- app/Components/WhatDoIDo/index.tsx
+++ app/Components/WhatDoIDo/index.tsx
@@ -14,21 +14,23 @@ export default function WhatDoIDo() {
       <div className="w-auto h-auto border-solid  border-4 border-b-red-600  bg-white">
         <div className=" flex justify-center flex-col items-center pt-10 ">
           <h1 className="text-red-600 text-5xl">O que eu faço</h1>
-          <p className="text-2xl text-black">Servicos e soluçoes</p>
+          <p className="text-2xl text-black">Serviços e soluções</p>
         </div>
 
         {/* Cards e imagens */}
 
         <div className="w-full h-full flex flex-col justify-center items-center  md:grid grid-cols-2  place-items-center
         xl:grid-cols-3">
-          <Cards Title="Sites para divugação de produtos fisicos" img={page} />
-          <Cards Title="lading pages" img={coffe} />
+          <Cards Title="Sites para divagação de produtos físicos" img={page} />
+          <Cards Title="landing page" img={coffe} />
           <Cards Title="sites pessoais" img={siteProfile} />
           <Cards Title="sites para produtos digitais" img={Finace} />
-          <Cards Title="Solucões para erros e bugs" img={Coding} />
+          <Cards Title="Soluções para erros e bugs" img={Coding} />
           <Cards Title="Mudanças e alterações no layout do seu site" img={Pricing} />
         </div>
       </div>
     </Element>
   );
 }
+
+
